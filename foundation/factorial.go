d4c41afc2007e5c06392a2e7ba7547dc6a753281
Shofyan <pencari.angin1@gmail.com>
2025-03-16 07:51:06 +0700

    init foundation for dsa

---
+++ foundation/factorial.go
@@ -0,0 +1,25 @@
+package main
+
+func Factorial(n int) int {
+
+	println("Factorial")
+	println("n: ", n)
+
+	if n == 0 {
+		return 1
+	}
+	return n * Factorial(n-1)
+}
+
+// factorial iterative
+func FactorialIterative(n int) int {
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
+	}
+	return result
+}
+
+func main() {
+	println("Factorial result: ", Factorial(4))
+}
